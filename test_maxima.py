cc0f02bea9a3021b4e4123640e08c026bfe97db8
Bence Bagi <bagi.bence94@gmail.com>
2018-04-10 10:04:41 +0200

    Organized inputs and outputs into lists

--- test_maxima.py
+++ test_maxima.py
@@ -11,23 +11,21 @@ def test_find_maxima(input, output):
 
 
 if __name__ == '__main__':
-    x1 = [0, 1, 2, 1, 2, 1, 0]
-    x2 = [-i**2 for i in range(-3, 4)]
-    x3 = [np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)]
-    x4 = [4, 2, 1, 3, 1, 2]
-    x5 = [4, 2, 1, 3, 1, 5]
-    x6 = [4, 2, 1, 3, 1]
+    inputs = list()
+    inputs.append([0, 1, 2, 1, 2, 1, 0])
+    inputs.append([-i**2 for i in range(-3, 4)])
+    inputs.append([np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)])
+    inputs.append([4, 2, 1, 3, 1, 2])
+    inputs.append([4, 2, 1, 3, 1, 5])
+    inputs.append([4, 2, 1, 3, 1])
 
-    y1 = [2, 4]
-    y2 = [3]
-    y3 = [16, 78]
-    y4 = [0, 3, 5]
-    y5 = [0, 3, 5]
-    y6 = [0, 3]
+    outputs = list()
+    outputs.append([2, 4])
+    outputs.append([3])
+    outputs.append([16, 78])
+    outputs.append([0, 3, 5])
+    outputs.append([0, 3, 5])
+    outputs.append([0, 3])
 
-    print(test_find_maxima(x1, y1))
-    print(test_find_maxima(x2, y2))
-    print(test_find_maxima(x3, y3))
-    print(test_find_maxima(x4, y4))
-    print(test_find_maxima(x5, y5))
-    print(test_find_maxima(x6, y6))
+    for input, output in zip(inputs, outputs):
+        print(test_find_maxima(input, output))
